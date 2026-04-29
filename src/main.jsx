14c40958c4e65469b6322c3687336a6363f53ba0
Shrinidhi <shrinidhibhat290@gmail.com>
2026-04-29 16:11:42 +0530

    Set up HashRouter for GitHub Pages and create contact page

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles/global.css';
-import { HashRouter } from 'react-router-dom';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>   {/* 👈 ADD THIS */}
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
